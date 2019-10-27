d6fc3142a49ce91c663c7bbe6ae6ce88d9d20f9b
solopointer <solopointer1202@gmail.com>
2019-10-27 17:45:43 +0800

    upload

--- include/ghead.h
+++ include/ghead.h
@@ -12,29 +12,37 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-namespace galois
+namespace galois::ghead
 {
-enum GALOIS_HEAD_FLAGS_T {
-    GALOIS_HEAD_CHECK_NONE = 0,
-    GALOIS_HEAD_CHECK_MAGICNUM = 0x01,
-    GALOIS_HEAD_CHECK_PARAM = 0x02,
+enum GHEAD_FLAGS_T {
+    GHEAD_CHECK_NONE = 0,
+    GHEAD_CHECK_MAGICNUM = 0x01,
+    GHEAD_CHECK_PARAM = 0x02,
 };
 
-enum GALOIS_HEAD_RET_ERROR_T {
-    GALOIS_HEAD_RET_SUCCESS =   0,
-    GALOIS_HEAD_RET_EPARAM =  -1,
-    GALOIS_HEAD_RET_EBODYLEN =  -2,
-    GALOIS_HEAD_RET_WRITE =  -3,
-    GALOIS_HEAD_RET_READ =  -4,
-    GALOIS_HEAD_RET_READHEAD =  -5,
-    GALOIS_HEAD_RET_WRITEHEAD =  -6,
-    GALOIS_HEAD_RET_PEARCLOSE =  -7,
-    GALOIS_HEAD_RET_ETIMEDOUT =  -8,
-    GALOIS_HEAD_RET_EMAGICNUM =  -9,
-    GALOIS_HEAD_RET_UNKNOWN =  -10
+enum RETURN_CODE {
+    SUCCESS = 0,
+    EPARAM = -1,
+    EBODYLEN = -2,
+    WRITE = -3,
+    READ = -4,
+    READHEAD = -5,
+    WRITEHEAD = -6,
+    PEARCLOSE = -7,
+    ETIMEDOUT = -8,
+    EMAGICNUM = -9,
+    UNKNOWN = -10
 };
 typedef int socket_t;
-struct galois_head 
+enum LOG_LEVEL {
+    FATAL,
+    ERROR,
+    WARNING,
+    INFO,
+    DEBUG,
+    TRACE
+};
+struct ghead 
 {
     // No virtual, No 'this' pointer, all static except
     unsigned short id;
@@ -47,12 +55,15 @@ struct galois_head
     unsigned int reserved3;
     unsigned int body_len;
 
-    static const unsigned int GALOIS_HEAD_MAGICNUM;
-    static GALOIS_HEAD_RET_ERROR_T read(socket_t sock, galois_head * head, void * req, size_t req_size,
+    static const std::map<RETURN_CODE, std::string> return_code_to_string;
+    static const unsigned int GHEAD_MAGICNUM;
+    const std::string & return_code_to_string(RETURN_CODE);
+    static void log(LOG_LEVEL loglevel, char * fmt, ...);
+    static RETURN_CODE read(socket_t sock, ghead * head, void * req, size_t req_size,
             void * buf, size_t buf_size, int timeout, unsigned flags);
     static ssize_t sync_read_n_tmo(socket_t fd, void * ptr1, size_t nbytes, int msecs);
-    static ssize_t reado_tv(socket_t fd, void * ptr, size_t nbytes, struct timeval * tv);
-    static int sreadable_tv(socket_t fd, struct timeval * tv);
+    static ssize_t read_n_tmo(socket_t fd, void * ptr, size_t nbytes, struct timeval * tv);
+    static int read_tmo(socket_t fd, struct timeval * tv);
     static int timeval_2_ms(const struct timeval * ptv);
     static void ms_2_timeval(const int msec, struct timeval * ptv);
     static int poll_wrap(struct pollfd * fdarray, unsigned int nfds, int timeout);
--- src/ghead.cpp
+++ src/ghead.cpp
@@ -1,53 +1,98 @@
 #include "ghead.h"
 #include <stdio.h>
+#include <stdarg.h>
 #include <boost/log/trivial.hpp>
+#include <chrono>
 
-namespace galois
+
+namespace galois::ghead
 {
-const unsigned int galois_head::GALOIS_HEAD_MAGICNUM = 0xe8c4a59;
+const unsigned int ghead::GHEAD_MAGICNUM = 0xe8c4a59;
 
-GALOIS_HEAD_RET_ERROR_T galois_head::read(int sock, galois_head * head, void *req, size_t req_size,
+const std::map<RETURN_CODE, std::string> ghead::return_code_to_string = {
+    {SUCCESS, "SUCCESS"},
+    {EPARAM, "EPARAM"}, 
+    {EBODYLEN, "EBODYLEN"},
+    {WRITE, "WRITE"},
+    {READ, "READ"},
+    {READHEAD, "READHEAD"},
+    {WRITEHEAD, "WRITEHEAD"},
+    {PEARCLOSE, "PEARCLOSE"},
+    {ETIMEDOUT, "ETIMEDOUT"},
+    {EMAGICNUM, "EMAGICNUM"},
+    {UNKNOWN, "UNKNOWN"},
+};
+
+const std::string & return_code_to_string(RETURN_CODE retcode)
+{
+    static const std::string dft = "CODEERROR";
+    auto iter = std::find(return_code_to_string.cbegin(), 
+        return_code_to_string.cend(), retcode);
+    if (iter == return_code_to_string.end()) {
+
+        return dft;
+    } else
+    {
+        return *iter;
+    }
+}
+
+void ghead::log(LOG_LEVEL loglevel, char * fmt, ...)
+{
+    va_list args;
+    char buf[1024];
+    va_start(args, fmt);
+    vsprintf(buf, fmt, args);
+    va_end(args);
+    switch(loglevel) {
+    case FATAL:BOOST_LOG_TRIVIAL(trace) << buf;break;
+    case DEBUG:BOOST_LOG_TRIVIAL(debug) << buf;break;
+    case INFO:BOOST_LOG_TRIVIAL(info) << buf;break;
+    case WARNING:BOOST_LOG_TRIVIAL(warning) << buf;break;
+    case ERROR:BOOST_LOG_TRIVIAL(error) << buf;break;
+    case FATAL:BOOST_LOG_TRIVIAL(fatal) << buf;break;
+    };
+}
+
+RETURN_CODE ghead::read(int sock, ghead * head, void *req, size_t req_size,
         void *buf, size_t buf_size, int timeout, unsigned flags)
 {
 
-    if (flags & GALOIS_HEAD_CHECK_PARAM) {
-        if (sock < 0 || !head)
-            return GALOIS_HEAD_RET_EPARAM;
-    }
+    if (sock < 0 || !head)
+        return EPARAM;
 
-    int rlen = sync_read_n_tmo(sock, head, sizeof(galois_head), timeout);
+    int rlen = sync_read_n_tmo(sock, head, sizeof(ghead), timeout);
+
     if (rlen <= 0)
-        goto nshead_read_fail;
-    else if (rlen != sizeof(galois_head)) {
-        printf("<%u> nshead_read head fail: ret %d want %u ERR[%m]",
+        goto ghead_read_fail;
+    else if (rlen != sizeof(ghead)) {
+        printf("<%u> ghead_read head fail: ret %d want %u ERR[%m]",
                 head->log_id, rlen, (unsigned int)req_size);
-        return GALOIS_HEAD_RET_READHEAD;
+        return GHEAD_RET_READHEAD;
     }
 
-    if (flags & GALOIS_HEAD_CHECK_MAGICNUM) {
-        if (head->magic_num != GALOIS_HEAD_MAGICNUM) {
-            printf("<%u> nshead_read magic num mismatch: ret %x want %x",
-                    head->log_id, head->magic_num, GALOIS_HEAD_MAGICNUM);
-            return GALOIS_HEAD_RET_EMAGICNUM;
-        }
+    if (head->magic_num != GHEAD_MAGICNUM) {
+        log(ERROR, "<%u> ghead_read magic num mismatch: ret %x want %x",
+                head->log_id, head->magic_num, GHEAD_MAGICNUM);
+        return EMAGICNUM;
     }
 
     /* 对 body 的长度进行检查, 过长和过短都不符合要求 */
     if (head->body_len < req_size || head->body_len - req_size > buf_size) {
-        printf("<%u> nshead_read body_len error: req_size=%u buf_size=%u body_len=%u",
+        printf("<%u> ghead_read body_len error: req_size=%u buf_size=%u body_len=%u",
                 head->log_id, (unsigned int)req_size, (unsigned int)buf_size, head->body_len);
-        return GALOIS_HEAD_RET_EBODYLEN;
+        return GHEAD_RET_EBODYLEN;
     }
 
     /* Read Request */
     if (req_size > 0) {
         rlen = sync_read_n_tmo(sock, req, req_size, timeout);
         if (rlen <= 0)
-            goto nshead_read_fail;
+            goto ghead_read_fail;
         else if (rlen != (int)req_size) {
-            printf("<%u> nshead_read fail: ret %d want %u ERR[%m]",
+            printf("<%u> ghead_read fail: ret %d want %u ERR[%m]",
                     head->log_id, rlen, (unsigned int)req_size);
-            return GALOIS_HEAD_RET_READ;
+            return GHEAD_RET_READ;
         }
     }
 
@@ -55,29 +100,31 @@ GALOIS_HEAD_RET_ERROR_T galois_head::read(int sock, galois_head * head, void *re
     if (head->body_len - req_size > 0) {
         rlen = sync_read_n_tmo(sock, buf, head->body_len - req_size, timeout);
         if (rlen <= 0)
-            goto nshead_read_fail;
+            goto ghead_read_fail;
         else if (rlen != (int)(head->body_len - req_size)) {
-            printf("<%u> nshead_read fail: ret %d want %d ERR[%m]",
+            printf("<%u> ghead_read fail: ret %d want %d ERR[%m]",
                     head->log_id, rlen, int(head->body_len - req_size));
-            return GALOIS_HEAD_RET_READ;
+            return GHEAD_RET_READ;
         }
     }
 
-    return GALOIS_HEAD_RET_SUCCESS;
+    return GHEAD_RET_SUCCESS;
 
-nshead_read_fail:
+ghead_read_fail:
     if (rlen == 0)
-        return GALOIS_HEAD_RET_PEARCLOSE;
+        return GHEAD_RET_PEARCLOSE;
 
-    printf("<%u> nshead_read fail: ret=%d ERR[%m]",head->log_id, rlen);
+    printf("<%u> ghead_read fail: ret=%d ERR[%m]",head->log_id, rlen);
     if (rlen == -1 && errno == ETIMEDOUT)
-        return GALOIS_HEAD_RET_ETIMEDOUT;
+        return GHEAD_RET_ETIMEDOUT;
     else
-        return GALOIS_HEAD_RET_READ;
+        return GHEAD_RET_READ;
 }
 
-ssize_t galois_head::sync_read_n_tmo(int fd, void *ptr1, size_t nbytes, int msecs)
+ssize_t ghead::sync_read_n_tmo(int fd, void *ptr1, size_t nbytes, int msecs)
 {
+    if (ptr1 == nullptr || nbytes == 0)
+        return -1;
     ssize_t nread;
     size_t nleft = nbytes;
     char * ptr = (char *) ptr1;
@@ -85,7 +132,7 @@ ssize_t galois_head::sync_read_n_tmo(int fd, void *ptr1, size_t nbytes, int msec
     struct timeval *tp;
 
     if (msecs < 0) {
-        tp = NULL;
+        tp = nullptr;
     } else {
         ms_2_timeval(msecs, &tv);
         tp = &tv;
@@ -115,7 +162,7 @@ ssize_t galois_head::sync_read_n_tmo(int fd, void *ptr1, size_t nbytes, int msec
     return (ssize_t)(nbytes - nleft);
 }
 
-ssize_t galois_head::reado_tv(int fd, void *ptr, size_t nbytes, struct timeval * tv)
+ssize_t ghead::reado_tv(int fd, void *ptr, size_t nbytes, struct timeval * tv)
 {
     int err;
     err = sreadable_tv(fd, tv);
@@ -128,71 +175,108 @@ ssize_t galois_head::reado_tv(int fd, void *ptr, size_t nbytes, struct timeval *
     }
 }
 
-int galois_head::sreadable_tv(int fd, struct timeval *tv)
-{
 
+ssize_t ghead::readn(int fd, void *vptr, size_t n)
+{
+	size_t	nleft;
+	ssize_t	nread;
+	char	*ptr;
+	ptr = vptr;
+	nleft = n;
+	while (nleft > 0) {
+		if ((nread = read(fd, ptr, nleft)) < 0) {
+			if (errno == EINTR)
+				nread = 0;		/* and call read() again */
+			else
+				return(-1);
+		} else if (nread == 0)
+			break;				/* EOF */
+
+		nleft -= nread;
+		ptr += nread;
+	}
+	return(n - nleft);		/* return >= 0 */
+}
+
+ssize_t ghead::read_n_tmo(int fd, void * ptr, size_t nbytes, int timeout_ms)
+{
+    if (ptr == nullptr || nbytes == 0)
+      return 0;
     pollfd pfd;
     pfd.fd = fd;
     pfd.events = POLLIN;
-
-    if ( tv == NULL ) {
-        return poll_wrap(&pfd, 1, -1);
-    } else {
-        struct timeval before, after;
-        gettimeofday(&before, NULL);
-        int result = (poll_wrap(&pfd, 1, timeval_2_ms(tv)));
-        gettimeofday(&after, NULL);
-
-        ul_sub_timeval(tv, &before, &after);
-
-        return result;
-    }
-
-}
-
-int galois_head::poll_wrap(struct pollfd *fdarray, unsigned int nfds, int timeout)
-{
-    int val, otimeout = timeout;
-    struct timeval ftv, btv;
-    gettimeofday(&ftv, NULL);
-    for (;;) {
-        val = poll(fdarray, nfds, timeout);
-        if (val < 0) {
-            if (errno == EINTR) {
-                if ( otimeout >= 0 ) {
-                    gettimeofday(&btv, NULL);
-                    timeout = otimeout - ((btv.tv_sec - ftv.tv_sec) * 1000 + (btv.tv_usec - ftv.tv_usec) / 1000);
-                    if ( timeout <= 0 ) {
-                        val = 0;
-                        errno = ETIMEDOUT;
-                        break;
-                    }
+    size_t nleft = nbytes;
+    while(nleft > 0) {
+        int result = poll_wrap(&pfd, 1, timeout_ms);
+        if (result > 0) {
+            int nread = readn(fd, ptr, nleft);
+            if (nread < 0) {
+                if (errno == EINTR) {
+                    nread = 0;
+                } else {
+                    return -1;
                 }
-                continue;
+            } else if (nread == 0) {
+                break;
             }
+            ptr += nread;
+            nleft -= nread;
         }
-        else if (val == 0) {
-            errno = ETIMEDOUT;
-        }
-        return (val);
+
     }
-    return (val);
 }
 
-void galois_head::ms_2_timeval(const int msec, struct timeval *ptv)
+// A simple wrap function of linux poll
+// fdarray : pollfd array
+// nfds : count of fds
+// timeout : timeout(ms) or (-1,0,>0)
+// return  -1, 0, nfds
+int ghead::poll_wrap(struct pollfd * fdarray, 
+    unsigned int nfds, int timeout_ms)
 {
-    if (ptv == NULL) {
-        return;
+    using namespace std::chrono;
+    if (fdarray == nullptr || nfds == 0)
+        return -1;
+    int ret_val = 0;
+    auto start = system_clock::now();
+    auto rest_timeout = milliseconds(timeout); 
+    while(true) {
+        ret_val = poll(fdarray, nfds, timeout);
+        if (ret_val > 0) {
+            return ret_val;
+        } else if (ret_val == 0) {
+            errno = ETIMEDOUT;
+            return ret_val;
+        } else if (ret_val < 0 && errno == EINTR) {
+            rest_timeout -= system_clock::now() - start; 
+            if (duration_cast<milliseconds>(rest_timeout).count() > 0) {
+                continue;
+            } else {
+                errno = ETIMEDOUT;
+                ret_val = 0;
+                return ret_val;
+            }
+        } else {
+            log(WARNING, "poll() error:[%d][%s]", errno, strerror(errno));
+            return ret_val;
+        }
     }
+    errno = ETIMEDOUT;
+    return ret_val;
+}
+
+void ghead::ms_2_timeval(const int msec, struct timeval *ptv)
+{
+    if (ptv == nullptr) 
+        return;
     ptv->tv_sec = msec / 1000;
     ptv->tv_usec = (msec % 1000) * 1000;
 }
 
-int galois_head::timeval_2_ms(const struct timeval *ptv)
+int ghead::timeval_2_ms(const struct timeval * ptv)
 {
-    if ( ptv == NULL ) {
+    if ( ptv == nullptr )
         return -1;
-    }
     return ptv->tv_sec * 1000 + ptv->tv_usec / 1000;
 }
 
